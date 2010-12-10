dc7354d82ca2cfc3771498e8e8ffc4ceefab3785
Lee Muro <lee@lee.local>
2010-12-09 21:31:29 -0500

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Skyblocks::Application.config.secret_token = '2b7182cac1e79fef83edf5dc283fbe45ce32601a08da67d4c20d6cc3cb43ecfefe6af620b13f58574d74461ecfcfbbf737b6096ac5aedca5391ea4368b554e53'
